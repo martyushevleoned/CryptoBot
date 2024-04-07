08bcb824acdaf3d684406ff1dfe585c854da096c
Martyushev Leoned <Lyonya.martyushev@mail.ru>
2024-04-07 11:28:18 +0500

    initial add

---
+++ telegramBot/src/main/java/project/TelegramBot.java
@@ -0,0 +1,23 @@
+package project;
+
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@Component
+public class TelegramBot extends TelegramLongPollingBot {
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+    }
+
+    @Override
+    public String getBotUsername() {
+        return null;
+    }
+    @Override
+    public String getBotToken() {
+        return null;
+    }
+}
